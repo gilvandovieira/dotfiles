b0dd7a429a2f8cedc2a0394606f5346262bd785d
Gilvando Lucas Vieira <gilvandolucasvieira@gmail.com>
2026-05-29 23:41:56 -0300

    update

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -4,7 +4,7 @@
 local mod = "SUPER"
 local terminal = "ghostty"
 local browser = "helium-browser"
-local file_manager = "nautilus"
+local file_manager = "dolphin"
 
 local function bind(keys, dispatcher, flags)
   hl.bind(keys, dispatcher, flags)
@@ -23,7 +23,6 @@ hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 hl.env("GDK_BACKEND", "wayland,x11,*")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("QT_STYLE_OVERRIDE", "kvantum")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("MOZ_ENABLE_WAYLAND", "1")
